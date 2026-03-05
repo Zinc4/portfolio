071c7df6cfca064935a1eb13d4023a722973a28f
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2026-03-05 20:11:36 +0700

    initial commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,20 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    var _mtm = (window._mtm = window._mtm || []);
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src =
+      "https://cdn.matomo.cloud/hanggorosetovercelapp.matomo.cloud/container_lymrWsIV.js";
+    s.parentNode.insertBefore(g, s);
+  }, []);
   return (
     <div>
       <Header />
